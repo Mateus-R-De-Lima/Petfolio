a651db856fd138c975ed32517cbc05cb041f2dd6
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-03-10 22:43:15 -0300

    Feat : Creating the Pet controller and request and post classes for the controller, I'm also using an enum to define the pet type.

---
+++ Petfolio.Api/Controllers/PetController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Petfolio.Communication.Requests;
+using Petfolio.Communication.Responses;
+
+namespace Petfolio.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PetController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisterPetJson),StatusCodes.Status201Created)]
+        public IActionResult Register(RequestRegisterPetJson request)
+        {
+            return Created();
+        }
+    }
+}
---
+++ Petfolio.Communication/Enuns/PetTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petfolio.Communication.Enuns
+{
+    public enum PetTypes
+    {
+        Dog = 0,
+        Cat = 1,
+
+    }
+}
